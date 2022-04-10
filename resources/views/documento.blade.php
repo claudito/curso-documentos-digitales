d104a28a74a20c9a6ab97405715d83b3aeb6c452
lclaudiooverall <luis.claudio@perutec.com.pe>
2022-04-09 19:31:15 -0500

    vista html documentos

--- resources/views/documento.blade.php
+++ resources/views/documento.blade.php
@@ -1,1 +1,12 @@
-Hola soy la vista de documentos
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Pagína de de Bienvenida :D</h1>
+</body>
+</html>
